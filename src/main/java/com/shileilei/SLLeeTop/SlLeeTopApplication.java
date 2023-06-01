fad5f23a1c1cdb6ad8e1e599f633c7d57d2aaa30
shileilei <shileilei@youzan.com>
2023-06-01 11:43:24 +0800

    init

---
+++ src/main/java/com/shileilei/SLLeeTop/SlLeeTopApplication.java
@@ -0,0 +1,13 @@
+package com.shileilei.SLLeeTop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlLeeTopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlLeeTopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shileilei/SLLeeTop/SlLeeTopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shileilei.SLLeeTop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlLeeTopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
