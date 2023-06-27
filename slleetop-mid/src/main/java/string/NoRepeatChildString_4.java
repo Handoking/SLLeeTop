eda4d2be8c99a0569eedbdb24b508835a60a30a7
handoking <langjie0425@163.com>
2023-06-28 00:17:21 +0800

    add NoRepeatChildString solution

--- slleetop-mid/src/main/java/string/NoRepeatChildString_4.java
+++ slleetop-mid/src/main/java/string/NoRepeatChildString_4.java
@@ -8,6 +8,11 @@ import java.util.Map;
  * @description: 无重复最长子串
  * @author: shileilei
  * @date: 2023-06-27 19:42
+ * 题解核心：滑动窗口
+ * 既能获取字符又能获取index，并且有映射关系，存取都是O(1),所以选择HashMap来存储
+ * 搞清楚什么时候start指针后移，以及多次重复的字符怎么办，就解了
+ * 1. 当出现重复字符时，start指针移动到重复字符的下一个，相当于除去了重复的数据
+ * 2. 每次移动后，扫过区域数据作废，重复的字符需要被put覆盖为窗口内的字符index
  **/
 public class NoRepeatChildString_4 {
 
@@ -19,17 +24,19 @@ public class NoRepeatChildString_4 {
         Map<Character, Integer> characterIndexMap =  new HashMap<>();
         for (int i=0;i<s.length();i++){
             if (characterIndexMap.containsKey(s.charAt(i))){
-                if (i-start>maxLength){
-                    maxStartIndex= start;
-                    maxEndIndex = i-1;
+                if (characterIndexMap.get(s.charAt(i))+1>start){
+                    start=characterIndexMap.get(s.charAt(i))+1;
                 }
-                start=characterIndexMap.get(s.charAt(i))+1;
-                continue;
             }
-            maxEndIndex = i;
+            if (i+1-start>maxLength){
+                maxLength = i-start+1;
+                maxStartIndex = start;
+                maxEndIndex = i;
+            }
             characterIndexMap.put(s.charAt(i), i);
+
         }
-        maxLength = maxEndIndex-maxStartIndex+1;
+
         System.out.println("maxLength:"+maxLength);
         System.out.println("maxStartIndex:"+maxStartIndex+",maxEndIndex:"+maxEndIndex);
         System.out.println("maxString:"+s.substring(maxStartIndex,maxEndIndex+1));
@@ -37,7 +44,7 @@ public class NoRepeatChildString_4 {
     }
 
     public static void main(String[] args) {
-        String s = "abcabcd";
+        String s = "aaaaaaaaaaaaaaa";
         lengthOfLongestSubstring(s);
     }
 }
